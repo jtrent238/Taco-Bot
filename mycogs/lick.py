9ef6f303c761c9af5dc05aeb876333ecff9616bc
Trent Patterson <jtrent238@outlook.com>
2017-08-16 13:07:48 -0400

    Add files via upload

--- mycogs/lick.py
+++ mycogs/lick.py
@@ -2,17 +2,17 @@ import discord
 from discord.ext import commands
 
 class jtrent238:
-    """Kiss Someone!"""
+    """I will lick anyone! >.<"""
 
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
-    async def kiss(self, user : discord.Member):
-        """Kiss Someone"""
+    async def lick(self, user : discord.Member):
+        """I will lick anyone! >.<"""
 
         #Your code will go here
-        await self.bot.say("You Kiss " + user.mention + " <3")
+        await self.bot.say("You Lick " + user.mention + "!")
 
 def setup(bot):
     bot.add_cog(jtrent238(bot))
